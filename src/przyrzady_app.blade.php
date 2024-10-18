75cda23a538998961f3dcb60b91f4d8f1aa26e11
Greg <grzejatol@gmail.com>
2024-10-18 11:52:50 +0100

    initial

---
+++ src/przyrzady_app.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="pl">
+<head>
+  @include('gtag')
+  <meta charset="utf-8">
+  <title>Przyrządy MCP</title>
+  <base href="{!! config('mcp3.SUBFOLDER', '') !!}/apps/przyrzady/">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="../../favicon.png">
+
+  {!! HTML::script('vendor2018/jquery/dist/jquery.min.js')!!}
+  {!! HTML::script('js/vendor/toastr/toastr.min.js') !!}
+  {!! HTML::style('js/dostawcyglowni/toastr/toastr.css') !!}
+
+  <style>
+
+    html {
+
+    }
+    html, body {
+      min-height: 100%;
+      padding: 4px;
+      width: 100%;
+    }
+    body {
+      /* https://goo.gl/vEVCQL */
+      background: rgb(197, 197, 197);
+    }
+    .tabelki_mikro table {  border-collapse: collapse;  page-break-inside:auto;  }  .tabelki_mikro td, .tabelki_mikro th {  border: 1px solid black;  padding: 3px;  }  .tabelki_mikro tr {  page-break-inside:avoid;  page-break-after:auto;  }
+  </style>
+
+  <script type="text/javascript">
+    var BASE = '<?php echo URL::to('/'); ?>'; var MCP = { KLASA: '<?php echo isset(Auth::user()->uprawnienie_systemu) ? Auth::user()->uprawnienie_systemu : "" ?>', LOGIN: '<?php echo isset(Auth::user()->login) ? Auth::user()->login : "" ?>', WYDZIAL: '<?php echo isset(Auth::user()->uprawnienie) ? Auth::user()->uprawnienie : "" ?>'}; var SUBFOLDER = '{!! config('mcp3.SUBFOLDER', '') !!}';
+  </script>
+
+  <link rel="stylesheet" href="styles.45025d567869bd018c4a.css"></head>
+<body>
+<app-root>Wczytuję aplikację ...</app-root>
+</body>
+
+</html>
